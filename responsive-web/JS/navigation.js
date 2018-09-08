df8514d20e96b12f6b38e0f0e280919a96df27c6
Hensgrej <hensgrej@gmail.com>
2018-09-08 11:20:03 -0500

    Menu principal con iconos y redirección a qrscan.html

--- responsive-web/JS/navigation.js
+++ responsive-web/JS/navigation.js
@@ -1,6 +1,12 @@
 const connectBtn = document.getElementById('connect-btn');
+const QRBtn = document.getElementById('qr');
 
 connectBtn.addEventListener('click', (e) => {
     event.preventDefault(e);
     window.location.assign('../responsive-web/first-view.html');
-})
+});
+
+QRBtn.addEventListener('click', (e) => {
+    event.preventDefault(e)
+    window.location.assign('../responsive-web/qrscan.html');
+});
