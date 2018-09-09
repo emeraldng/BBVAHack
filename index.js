69ba6da7f8459bef9d49767bc23c1ce2875c5d39
YocelinGRomero <garcia_romero.y@hotmail.com>
2018-09-09 03:32:31 -0500

    Cambios para montar servidor 7

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Llamado a las instancias que se ocuparán-> Llamado a módulos
-const express = require('express');
+/* express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes/routes.js');
 const app = express();
@@ -18,4 +18,15 @@ const port = process.env.PORT || 3000;
 // Indoca en qué puerto correra la app y la inicia
 const server = app.listen(port, () => {
   console.log('app running on port.', server.address().port);
-}); // comments
+}); // comments */
+
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/responsive-web/public/login.html'));
+});
+
+app.listen(3000);
